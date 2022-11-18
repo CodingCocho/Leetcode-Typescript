3228bfbe339e903dc0021213832794c3e78984d8
CodingCocho <raulujr@gmail.com>
2022-11-17 22:21:28 -0800

    Added today's problem.

--- linked-lists/index.ts
+++ linked-lists/index.ts
@@ -330,88 +330,75 @@ function reverseList(head: ListNode | null): ListNode | null {
  * }
  */
 
- function removeElements(head: ListNode | null, val: number): ListNode | null 
- {
-   // No head edge case
-   if(!head)
-   {
-     return null
-   }
-   
-   // Head only edge case
-   if(head.next === null)
-   {
-     // Check if head needs to be deleted
-     if(head.val === val)
-     {
-       return null;
-     }
-     else
-     {
-       return head;
-     }
-   }
-   
-   // Head is val edge case loop
-   while(head.val === val)
-   {
-     head = head.next;
-     if(!head)
-     {
-       return null;
-     }
-   }
-   
-   // Hold our starting traversing node
-   let traversingNode: ListNode | null = head;
-   
-   // Traverse the LinkedList to find the desired ListNode
-   while(traversingNode.next !== null)
-   {
-     // Check if the next value 
-     if(traversingNode.next.val === val)
-     {
-       // Hold the following node
-       let temporaryNode: ListNode | null = traversingNode.next.next;
-       // Set the new next node
-       traversingNode.next = temporaryNode;
-       // Check we removed the tail
-       if(temporaryNode === null)
-       {
-         return head;
-       }
-     }
-     // Traverse the linked list
-     else
-     {
-       traversingNode = traversingNode.next;
-     }
-   }
-   // Return the head
-   return head;
- };
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+  // No head edge case
+  if (!head) {
+    return null
+  }
 
- /**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+  // Head only edge case
+  if (head.next === null) {
+    // Check if head needs to be deleted
+    if (head.val === val) {
+      return null;
+    }
+    else {
+      return head;
+    }
+  }
+
+  // Head is val edge case loop
+  while (head.val === val) {
+    head = head.next;
+    if (!head) {
+      return null;
+    }
+  }
+
+  // Hold our starting traversing node
+  let traversingNode: ListNode | null = head;
+
+  // Traverse the LinkedList to find the desired ListNode
+  while (traversingNode.next !== null) {
+    // Check if the next value 
+    if (traversingNode.next.val === val) {
+      // Hold the following node
+      let temporaryNode: ListNode | null = traversingNode.next.next;
+      // Set the new next node
+      traversingNode.next = temporaryNode;
+      // Check we removed the tail
+      if (temporaryNode === null) {
+        return head;
+      }
+    }
+    // Traverse the linked list
+    else {
+      traversingNode = traversingNode.next;
+    }
+  }
+  // Return the head
+  return head;
+};
+
+/**
+* Definition for singly-linked list.
+* class ListNode {
+*     val: number
+*     next: ListNode | null
+*     constructor(val?: number, next?: ListNode | null) {
+*         this.val = (val===undefined ? 0 : val)
+*         this.next = (next===undefined ? null : next)
+*     }
+* }
+*/
 
 function oddEvenList(head: ListNode | null): ListNode | null {
   // No head edge case
-  if(!head)
-  {
+  if (!head) {
     return null
   }
   // Only head edge case
-  if(!head.next)
-  {
+  if (!head.next) {
     return head;
   }
   // Hold the length of the even ListNode link
@@ -421,30 +408,27 @@ function oddEvenList(head: ListNode | null): ListNode | null {
   // Hold the pointer the first even ListNode
   let evenPointer: ListNode = head.next;
   // Loop through the LinkedList
-  while(true)
-  {
-    
+  while (true) {
+
     // Retrieve potentialNode
     let lengthCounter: number = 0;
-    
+
     // Hold the next odd ListNode
     let nextOddPointer: ListNode | null = evenPointer;
-    
+
     // Hold the last even ListNode
     let lastEvenPointer: ListNode | null = oddPointer;
-    
+
     // Traverse the even ListNode link
-    while(lengthCounter !== lengthOfEvenLink)
-    {
-      if(nextOddPointer.next === null)
-      {
+    while (lengthCounter !== lengthOfEvenLink) {
+      if (nextOddPointer.next === null) {
         return head;
       }
       lastEvenPointer = lastEvenPointer.next;
       nextOddPointer = nextOddPointer.next;
       lengthCounter++
     }
-  
+
     // Linking
     lastEvenPointer.next = nextOddPointer.next;
     oddPointer.next = nextOddPointer;
@@ -452,7 +436,7 @@ function oddEvenList(head: ListNode | null): ListNode | null {
     oddPointer = nextOddPointer;
     lengthOfEvenLink++;
   }
-  
+
 };
 
 /**
@@ -467,34 +451,93 @@ function oddEvenList(head: ListNode | null): ListNode | null {
  * }
  */
 
- function oddEvenList(head: ListNode | null): ListNode | null {
+function oddEvenList(head: ListNode | null): ListNode | null {
   if (!head || !head.next) {
-       return head;
-   }
- 
-   let oddList = head;
-   let evenList = head.next;
+    return head;
+  }
 
-   let curOdd = oddList;
-   let curEven = evenList;
- 
- let curNode = head.next.next;
-   let isOdd = true;
- 
-   while (curNode) {
-       if (isOdd) {
-           curOdd.next = curNode;
-           curOdd = curOdd.next;
-       } else {
-           curEven.next = curNode;
-           curEven = curEven.next;
-       }
-       curNode = curNode.next;
-       isOdd = !isOdd;
-   }
-   curEven.next = null;
-   curOdd.next = evenList;
- 
-   return oddList;
- 
+  let oddList = head;
+  let evenList = head.next;
+
+  let curOdd = oddList;
+  let curEven = evenList;
+
+  let curNode = head.next.next;
+  let isOdd = true;
+
+  while (curNode) {
+    if (isOdd) {
+      curOdd.next = curNode;
+      curOdd = curOdd.next;
+    } else {
+      curEven.next = curNode;
+      curEven = curEven.next;
+    }
+    curNode = curNode.next;
+    isOdd = !isOdd;
+  }
+  curEven.next = null;
+  curOdd.next = evenList;
+
+  return oddList;
+
+};
+
+function isPalindrome(head: ListNode | null): boolean {
+
+  // Theory you can do this in O(n) time complexity however we at most need O(n) space complexity since it is
+  // a singly linked list to account for the O(1) space of adding a prev pointer.
+  // Futhermore our length loop is irrelevant in a doubly linked list since we are given the tail node. 
+  // The challenge is using O(n) space
+  // My as well go with just pushing them all into an array. I was thinking unshifting and shifting since O(3n) > O(2n)
+
+  // Single node edge case
+  if (!head.next) {
+    return true;
+  }
+  // Two node edge case
+  if (!head.next.next) {
+    if (head.val !== head.next.val) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  // Hold the traversing node
+  let traversingNode: ListNode | null = head;
+  // Hold the length of the LinkedList
+  let length: number = 0;
+  // Hold an array of the LinkedList
+  let listNums: number[] = [];
+  // Add each value into an array
+  while (traversingNode) {
+    listNums.push(traversingNode.val);
+    traversingNode = traversingNode.next;
+    length++
+  }
+  // Hold the two pointers
+  let leftPointer: number = 0;
+  let rightPointer: number = 0;
+
+  // Check if the length is even
+  if (length % 2 === 0) {
+    rightPointer = length / 2;
+    leftPointer = rightPointer - 1;
+  }
+  // Check if the length is odd
+  else {
+    rightPointer = Math.floor(length / 2);
+    leftPointer = rightPointer;
+  }
+  // Traverse our array finding the condition that breaks the palindrome
+  while (leftPointer !== -1 && rightPointer !== listNums.length) {
+    if (listNums[leftPointer] !== listNums[rightPointer]) {
+      return false;
+    }
+    leftPointer--;
+    rightPointer++;
+  }
+  return true;
 };
